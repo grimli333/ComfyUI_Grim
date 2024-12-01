1845325219007a9a3c3789b54b7424d605ab482c
Scott Phillips <scott@jedi.net>
2024-12-01 12:09:32 -0600

    Adding GenerateFileName node to create a unique path/file name for the duration of a run

--- grim.py
+++ grim.py
@@ -1,6 +1,34 @@
 from typing import Optional, List
 from dataclasses import dataclass
 from folder_paths import get_folder_paths
+from datetime import datetime
+
+class GenerateFileName:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": {
+            "prefix": ("STRING", ),
+            "folder": ("STRING", {"default": ""}),
+        }}
+    
+    RETURN_TYPES = ("STRING","STRING","STRING")
+    RETURN_NAMES = ("full_pathname", "prefix_only", "folder_only")
+
+    FUNCTION = "generate_filename"
+
+    @classmethod
+    def IS_CHANGED(s, prefix):
+        return float("NaN")
+
+    def generate_filename(self, prefix):
+        timestamp = datetime.now().strftime("@%Y%m%d-%H%M%S")
+        if(self.folder != ""):
+            folder = self.folder
+        else:
+            folder = "{prefix}_{timestamp}".format(prefix=prefix, timestamp=timestamp)
+
+        full_pathname = "{folder}/{prefix}_{timestamp}".format(folder=folder, prefix=prefix, timestamp=timestamp)
+        return (full_pathname, prefix, folder)
 
 class TwoStringsFormat:
     @classmethod
@@ -27,9 +55,11 @@ class TwoStringsFormat:
 
 
 NODE_CLASS_MAPPINGS = {
+    "GenerateFileName": GenerateFileName,
     "TwoStringsFormat": TwoStringsFormat,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "GenerateFileName": "Generate a unique filename and folder name",
     "TwoStringsFormat": "Format Strings with Two Inputs",
 }
